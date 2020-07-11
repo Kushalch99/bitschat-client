f7ddad9ce0ed6130b1669e3dda6ee98a13b10b63
Kushal Choudhary <kushalch99@gmail.com>
2020-07-11 10:26:06 +0530

    profile section completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,19 +4,34 @@ import { Link } from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
+import { connect } from 'react-redux'
+import { logoutUser } from '../redux/actions/userActions'
 
 export class Navbar extends Component {
+    handleLogout = (event) => {
+        this.props.logoutUser()
+        window.location.href = '/login'
+    }
     render() {
+        const { user: { authenticated }} = this.props
         return (
             <AppBar>
                 <Toolbar className="nav-container">
-                    <Button color="inherit" component={Link} to="/login">Login</Button>
-                    <Button color="inherit" component={Link} to="/">Home</Button>
-                    <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                { !authenticated &&  <Button color="inherit" component={Link} to="/login" variant="outlined">Login</Button> }
+                &nbsp;&nbsp;
+                <Button color="inherit" component={Link} to="/" variant="outlined">Home</Button>
+                &nbsp;&nbsp;
+                { !authenticated && <Button color="inherit" component={Link} to="/signup" variant="outlined">Signup</Button> }
+                &nbsp;&nbsp;
+                { authenticated && <Button color="inherit" onClick={this.handleLogout} variant="outlined">Logout</Button> }
                 </Toolbar>
             </AppBar>
         )
     }
 }
 
-export default Navbar
+const mapStateToProps = (state) => ({
+    user: state.user
+})
+
+export default connect(mapStateToProps,{ logoutUser })(Navbar)
