1625519642a00f5b2ff9ffca72dfb6734dc73963
Kushal Choudhary <kushalch99@gmail.com>
2020-08-30 21:23:16 +0530

    like wip

--- client/src/components/Scream.js
+++ client/src/components/Scream.js
@@ -8,14 +8,24 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
 
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import CommentIcon from '@material-ui/icons/Comment'
 const styles = {
-  card: {
-    display: 'flex',
+  scream: {
     marginBottom: 20
+  },
+  card: {
+    display: 'flex'
   },
   image: {
     minWidth: 200
+  },
+  buttonGroup: {
+    marginTop: 20,
+    marginBottom: 0
   },
   content: {
     padding: 25,
@@ -28,27 +38,49 @@ class Scream extends Component {
     const { classes, scream } = this.props
     //const classes = this.props.classes
     return (
-      <Card className={classes.card}>
-        <CardMedia
-          image={scream.user.profileImageUrl}
-          title="Profile Image"
-          className={classes.image}
-        />
-        <CardContent className={classes.content}>
-          <Typography
-            variant="h5"
-            component={Link}
-            to={`/users/${scream.user.handle}`}
-            color="primary"
-          >
-            {scream.user.handle}
-          </Typography>
-          <Typography variant="body2" color="textSecondary">
-            {dayjs(scream.createdAt).fromNow()}
-          </Typography>
-          <Typography variant="body1">{scream.body}</Typography>
-        </CardContent>
-      </Card>
+      <div className={classes.scream}>
+        <Card className={classes.card}>
+          <CardMedia
+            image={scream.user.profileImageUrl}
+            title="Profile Image"
+            className={classes.image}
+          />
+          <CardContent className={classes.content}>
+            <Typography
+              variant="h5"
+              component={Link}
+              to={`/users/${scream.user.handle}`}
+              color="primary"
+            >
+              {scream.user.handle}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              {dayjs(scream.createdAt).fromNow()}
+            </Typography>
+            <Typography variant="body1">{scream.body}</Typography>
+            <ButtonGroup
+              color="secondary"
+              aria-label="outlined secondary button group"
+              className={classes.buttonGroup}
+            >
+              <Button
+                color="primary"
+                variant="outlined"
+                startIcon={<FavoriteBorderIcon />}
+              >
+                Like
+              </Button>{' '}
+              <Button
+                color="primary"
+                variant="outlined"
+                startIcon={<CommentIcon />}
+              >
+                Comment
+              </Button>{' '}
+            </ButtonGroup>
+          </CardContent>
+        </Card>
+      </div>
     )
   }
 }
